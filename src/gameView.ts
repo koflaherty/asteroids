1484b58e656acc02b1b7547d6a34ddf7490f8f7d
Kevin O'Flaherty <koflaherty@ymail.com>
2023-12-08 10:00:04 -0500

    Added a viewport library and played around with it

--- src/gameView.ts
+++ src/gameView.ts
@@ -1,4 +1,5 @@
 import { Application, Sprite } from 'pixi.js'
+import {Viewport} from "pixi-viewport";
 const bunny = Sprite.from('https://pixijs.com/assets/bunny.png')
 
 export function setupGameView(element: HTMLDivElement) {
@@ -7,7 +8,31 @@ export function setupGameView(element: HTMLDivElement) {
     resizeTo: window,
   });
 
-  app.stage.addChild(bunny)
+  // create viewport
+  const viewport = new Viewport({
+    screenWidth: window.innerWidth,
+    screenHeight: window.innerHeight,
+    worldWidth: 3000,
+    worldHeight: 3000,
+    events: app.renderer.events // the interaction module is important for wheel to work properly when renderer.view is placed or scaled
+  })
+
+// add the viewport to the stage
+  app.stage.addChild(viewport)
+
+// activate plugins
+  viewport
+  .drag()
+  .decelerate()
+  .clamp({
+    direction: "all",
+  });
+  viewport.x = -100
+  viewport.y = -100
+
+
+
+  viewport.addChild(bunny)
   // center the sprite's anchor point
   bunny.anchor.set(0.5)
 
@@ -18,8 +43,6 @@ export function setupGameView(element: HTMLDivElement) {
   element.appendChild(app.view);
 
   app.ticker.add((delta) => {
-    bunny.x = app.screen.width / 2
-    bunny.y = app.screen.height / 2
     // just for fun, let's rotate mr rabbit a little
     // delta is 1 if running at 100% performance
     // creates frame-independent transformation
